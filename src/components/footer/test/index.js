d0a01aefced965dc8c75e0842f0317bfd41de491
Imran Sayed <imranhsayed@gmail.com>
2020-04-23 22:33:06 +0530

    Update readme

--- src/components/footer/test/index.js
+++ src/components/footer/test/index.js
@@ -16,7 +16,21 @@ import Footer from '../index';
 
 describe( 'Test Footer Component', () => {
 	it( 'Should match Footer snapshot and render correctly', () => {
-		const component = renderer.create( <Footer /> );
+
+		const data= {
+			footer: {
+				copyrightText: '',
+				footerMenuItems: '',
+				socialLinks: [],
+				sidebarTwo: '',
+				sidebarOne: '',
+			},
+		};
+		const loading = false;
+		const errorMsg = true;
+
+
+		const component = renderer.create( <Footer footerData={ { data, loading, errorMsg } }/> );
 
 		const tree = component.toJSON();
 
