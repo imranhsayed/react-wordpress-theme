f25f91b2cec5b132673a22599416c7af070ab27c
Imran Sayed <imranhsayed@gmail.com>
2020-04-23 22:18:59 +0530

    Add css

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -18,48 +18,50 @@ const Footer = ( props ) => {
 	return (
 		<div className="footer">
 
-			{/*Top section*/}
-			<div className="footer__top">
-				{ sidebarOne ? <div  dangerouslySetInnerHTML={ { __html: sidebarOne } } className="footer__sidebar-one"/> : '' }
-				{ sidebarTwo ? <div  dangerouslySetInnerHTML={ { __html: sidebarTwo } } className="footer__sidebar-two"/> : '' }
-				{
-					footerMenuItems.length ? (
-						<div className="footer-menu-items">
-							{ config.footerMenuTitle ? <h6>{ config.footerMenuTitle }</h6> : '' }
-							<ul>
-								{ footerMenuItems.map( menu  => (
-									<li key={ menu.ID }>
-										<Link
-											className="header-nav__menu-link"
-											to={ `/${ menu.pageSlug }` }
-										>
-											{ menu.title }
-										</Link>
-									</li>
-								)) }
-							</ul>
-						</div>
-					) : ''
-				}
-			</div>
+			<div className="wrapper">
+				{/*Top section*/}
+				<div className="footer__top">
+					{ sidebarOne ? <div  dangerouslySetInnerHTML={ { __html: sidebarOne } } className="footer__sidebar-one"/> : '' }
+					{ sidebarTwo ? <div  dangerouslySetInnerHTML={ { __html: sidebarTwo } } className="footer__sidebar-two"/> : '' }
+					{
+						footerMenuItems.length ? (
+							<div className="footer-menu-items">
+								{ config.footerMenuTitle ? <h6>{ config.footerMenuTitle }</h6> : '' }
+								<ul>
+									{ footerMenuItems.map( menu  => (
+										<li key={ menu.ID }>
+											<Link
+												className="header-nav__menu-link"
+												to={ `/${ menu.pageSlug }` }
+											>
+												{ menu.title }
+											</Link>
+										</li>
+									)) }
+								</ul>
+							</div>
+						) : ''
+					}
+				</div>
 
-		{/*	Bottom section*/}
-		<div className="footer__bottom">
-			{ copyrightText ? <div className="copyright-text">{ copyrightText }</div> : config.copyrightText }
-			{
-				socialLinks.length ?
-					<ul className="social-links">
-						{ socialLinks.map( socialLink => (
-							<li key={ socialLink.iconName }>
-								<a href={ socialLink.iconUrl } target="_blank">
-									<i className={ `fa fa-${socialLink.iconName}` }/>
-								</a>
-							</li>
-						) ) }
-					</ul> :
-					''
-			}
-		</div>
+				{/*	Bottom section*/}
+				<div className="footer__bottom">
+					{ copyrightText ? <div className="copyright-text">{ copyrightText }</div> : config.copyrightText }
+					{
+						socialLinks.length ?
+							<ul className="social-links">
+								{ socialLinks.map( socialLink => (
+									<li key={ socialLink.iconName }>
+										<a href={ socialLink.iconUrl } target="_blank">
+											<i className={ `fa fa-${socialLink.iconName}` }/>
+										</a>
+									</li>
+								) ) }
+							</ul> :
+							''
+					}
+				</div>
+			</div>
 		</div>
 	);
 };
